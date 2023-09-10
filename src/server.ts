9633e1529cdbb4995635ac5c3a353fcb7819a04e
Davi Alves <cadwvk@gmail.com>
2023-09-10 15:09:31 -0300

    Added eslint

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { app } from "./app";
-import { env } from "./env";
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-  host: '0.0.0.0',
-  port: env.PORT
-}).then(() => {
-  console.log('Servidor rodando em http://localhost:3333')
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Servidor rodando em http://localhost:3333')
+  })

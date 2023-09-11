bc9c5aaad6c5869d9b1289fc0e8ee6ee162780a9
Davi Alves <cadwvk@gmail.com>
2023-09-11 19:49:12 -0300

    Added paginate in check-ins

--- src/use-cases/fetch-user-check-ins-history.ts
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -3,6 +3,7 @@ import { CheckIn } from '@prisma/client'
 
 interface FetchUserCheckInsHistoryUseCaseRequest {
   userId: string
+  page: number
 }
 
 interface FetchUserCheckInsHistoryUseCaseResponse {
@@ -14,8 +15,12 @@ export class FetchUserCheckInsHistoryUseCase {
 
   async execute({
     userId,
+    page,
   }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
-    const checkIns = await this.checkInsRepository.findManyByUserId(userId)
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
 
     return { checkIns }
   }

97c701df0615c2a8adeeeb38332d6253246c8f88
Davi Alves <cadwvk@gmail.com>
2023-09-11 15:45:05 -0300

    Created 'should not be able to authenticate with wrong email' test

--- src/use-cases/authenticate.spec.ts
+++ src/use-cases/authenticate.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user
 import { hash } from 'bcryptjs'
 import { describe, expect, it } from 'vitest'
 import { AuthenticateUseCase } from './authenticate'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -20,5 +21,17 @@ describe('Authenticate Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    expect(() =>
+      sut.execute({
+        email: 'teste@gmail.com',
+        password: '15s6f89w',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })

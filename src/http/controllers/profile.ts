839e5d27e35267df27e24fc6221e9ad3ae81864f
Davi Alves <cadwvk@gmail.com>
2023-09-12 14:34:22 -0300

    Added jwtVerify in profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,6 +1,7 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(req: FastifyRequest, res: FastifyReply) {
+  await req.jwtVerify()
 
   return res.status(200).send()
 }

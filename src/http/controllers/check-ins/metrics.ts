f8c64a242a5c73ab57560d95d4845ccda306de2a
Davi Alves <cadwvk@gmail.com>
2023-09-12 17:37:16 -0300

    Created check-in metrics controller

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,14 @@
+import { makeGetUserMetricsUseCase } from '@/use-cases/factories/make-get-user-metrics-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
+
+export async function history(req: FastifyRequest, res: FastifyReply) {
+  const getUserMetricsUseCase = makeGetUserMetricsUseCase()
+
+  const { checkInsCount } = await getUserMetricsUseCase.execute({
+    userId: req.user.sub,
+  })
+
+  return res.status(201).send({
+    checkInsCount,
+  })
+}
